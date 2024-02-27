26969faf1bc23cab414dc9ac9d3d68348719de92
RafaelPorto <rafaelmportto@gmail.com>
2024-02-26 21:53:57 -0300

    Initial commit

---
+++ src/test/java/com/rafaelPorto/automationTestWithSelenium/AutomationTestWithSeleniumApplicationTests.java
@@ -0,0 +1,9 @@
+package com.rafaelPorto.automationTestWithSelenium;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTestWithSeleniumApplicationTests {
+
+
+}
